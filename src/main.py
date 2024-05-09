eb22afe8d803d3ad4cfab51d5ce005846b32477c
Georgii Ovechkin <goqwertys@gmail.com>
2024-05-09 08:25:01 +0400

    moded pyproject.toml configs and main.py updated

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 """
 Hello World
 """
+
 from src.hello_world import greetings
+
+
 def main():
+    name = input("What is your name?\n")
     greetings("Pavel")
+
 
 if __name__ == "__main__":
     main()
